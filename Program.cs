6308e039f6cfee962aad7535af3b1e3908bebf98
Ronan Donnelly <ronan.donnelly@ymail.com>
2026-06-16 14:32:20 +0100

    Initial commit: DesertIsland C# console adventure

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("You wash up on a deserted island.");
+        Console.WriteLine("In the distance, a figure approaches you.");
+    }
+}
